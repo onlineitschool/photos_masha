7379d9ef8436b0a4a230845ccd7cd6057c3f0d9b
posicel <121557709+posicel@users.noreply.github.com>
2023-03-20 16:57:40 +0200

    Update app.py
    making the same file extensions

--- app.py
+++ app.py
@@ -4,14 +4,10 @@ from PIL import Image
 print ('Введите имя папки с фотографиями')
 input_dir_name = input()
 photos = os.listdir(path='./' + input_dir_name)
-print (photos)
 
 import datetime
 dt_now = datetime.datetime.now()
-# print('dt_now', type(dt_now))
 dt_now_str = str(dt_now)
-# print('dt_now_str', type(dt_now_str))
-# print(dt_now)
 
 now = dt_now_str.split(' ')[0]+'_'+dt_now_str.split(' ')[1].split(':')[0]+'-'+dt_now_str.split(' ')[1].split(':')[1]
 
@@ -21,15 +17,14 @@ os.mkdir('icons_'+ now)
 number = 1
 for photo in photos:
     old_address_photo = './' + input_dir_name + '/' + photo
-    shutil.copyfile(old_address_photo, 'photos_'+ now + '/' + 'photo_' + str(number))
+    ext = photo.split('.')[-1]
+    shutil.copyfile(old_address_photo, 'photos_'+ now + '/' + 'photo_' + str(number) + '.' + ext)
     
     #read the image 
     im = Image.open(old_address_photo) 
 
     width = im.size[0] 
     height = im.size[1] 
-    print('Width of the image is:', width)
-    print('Height of the image is:', height)
 
     #image size
     icon_width = 100 
@@ -39,6 +34,6 @@ for photo in photos:
     out = im.resize(size) 
 
     #save resized image 
-    out.save('icons_'+ now + '/' + 'icon_' + str(number) + '.png')
+    out.save('icons_'+ now + '/' + 'icon_' + str(number) + '.' + ext)
 
     number += 1
