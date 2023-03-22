f49fad334f67c4ebb5c8b0ef086885e106d77157
posicel <121557709+posicel@users.noreply.github.com>
2023-03-22 20:44:46 +0200

    With MySQL

--- app.py
+++ app.py
@@ -14,11 +14,23 @@ now = dt_now_str.split(' ')[0]+'_'+dt_now_str.split(' ')[1].split(':')[0]+'-'+dt
 os.mkdir('photos_'+ now) 
 os.mkdir('icons_'+ now)
 
+all_params = []
 number = 1
 for photo in photos:
     old_address_photo = './' + input_dir_name + '/' + photo
     ext = photo.split('.')[-1]
     shutil.copyfile(old_address_photo, 'photos_'+ now + '/' + 'photo_' + str(number) + '.' + ext)
+
+    #request = "INSERT INTO `photos` (`folder`, `file`, `user_folder`, `user_file`, `tags`, `comment`)
+    # params = ('photoDDMMYYYY', 'photo1', 'My photos', 'mum.png', None, None) 
+    temp_dict = {}
+    temp_dict['folder'] = 'photos_'+ now
+    temp_dict['file'] = 'photo_' + str(number) + '.' + ext
+    temp_dict['user_folder'] = input_dir_name
+    temp_dict['user_file'] = photo
+    temp_dict['tags'] = None
+    temp_dict['comment'] = None
+    all_params.append(temp_dict)
     
     #read the image 
     im = Image.open(old_address_photo) 
@@ -37,3 +49,47 @@ for photo in photos:
     out.save('icons_'+ now + '/' + 'icon_' + str(number) + '.' + ext)
 
     number += 1
+print (all_params)
+
+import mysql.connector
+
+def connection_db():
+    try:
+        db_connection = mysql.connector.connect(
+
+            host="mysql.s1.users24.com",
+            port="30096",
+            user="root",
+            password = 'lkjsfdjklds3248723487BNXNVKHVHweoiwgiwelklkwdsqpsdkfljdls',
+            database="masha_photos"
+        )    
+    except Exception as e:
+        print(str(e))
+
+    return db_connection
+
+def requests_to_db(params=None): 
+    connection = connection_db()
+
+    #здесь один запрос, но их надо отправлять в цикле
+    mycursor = connection.cursor(dictionary=True)
+
+    for param in all_params:
+
+        folder = param['folder']
+        file_name = param['file']
+        user_folder = param['user_folder']
+        user_file = param['user_file']
+        tags = param['tags']
+        comment = param['comment']
+
+
+        request = "INSERT INTO `photos` (`folder`, `file`, `user_folder`, `user_file`, `tags`, `comment`) VALUES (%s, %s, %s, %s, %s, %s);"
+        params = (folder, file_name, user_folder, user_file, tags, comment)
+    
+        mycursor.execute(request, params)
+
+         # Это завершение работы с БД и сохранение изменений
+    connection.commit()
+
+requests_to_db(all_params)
